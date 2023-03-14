69d8c82bf76d715854250795f0944478ea6f69bc
Ryuuz02 <ryuuz02@outlook.com>
2023-03-14 17:23:20 -0500

    first commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		boolean hello = true;
+		if (hello) {
+			System.out.println("Hello World!");
+		}
+
+	}
+
+}
